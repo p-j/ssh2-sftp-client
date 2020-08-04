f22f2449119c82d5ac3dfad6be81a877a2094cb7
Jérémie Parker <github@jeremie-parker.com>
2020-08-04 09:58:35 +0200

    low level buffer to stream conversion

--- tools/lowlevel-buffer-put.js
+++ tools/lowlevel-buffer-put.js
@@ -7,6 +7,20 @@ const dotenvPath = path.join(__dirname, '..', '.env');
 require('dotenv').config({path: dotenvPath});
 
 const Client = require('ssh2');
+const { Readable } = require('stream');
+
+function bufferToStream(buffer) {
+  const stream = new Readable();
+  stream.on('close', () => console.log('read stream close'));
+  stream.on('data', () => console.log('read stream data'));
+  stream.on('end', () => console.log('read stream end'));
+  stream.on('error', () => console.log('read stream error'));
+  stream.on('pause', () => console.log('read stream pause'));
+  stream.on('resume', () => console.log('read stream resume'));
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+}
 
 const config = {
   host: process.env.SFTP_SERVER,
@@ -35,10 +49,14 @@ client
           console.log(`File successfully uploaded to ${remotePath}`);
           client.end();
         });
+        stream.on('drain', () => console.log('write stream drain'));
+        stream.on('pipe', () => console.log('write stream pipe'));
+        stream.on('unpipe', () => console.log('write stream unpipe'));
         let bufSize = 1024 * size;
         let buf = Buffer.alloc(bufSize);
         console.log(`Uploading buffer of ${bufSize} bytes to ${remotePath}`);
-        stream.end(buf);
+        bufferToStream(buf).pipe(stream);
+        // stream.end();
       }
     });
   })
